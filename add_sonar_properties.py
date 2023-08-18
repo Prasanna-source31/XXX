713700b959d1abcbe82d35c5ca50395f5d8324ad
Prasanna-source31 <139847095+Prasanna-source31@users.noreply.github.com>
2023-08-18 16:44:53 +0530

    Update add_sonar_properties.py

--- add_sonar_properties.py
+++ add_sonar_properties.py
@@ -8,7 +8,7 @@ access_token = os.environ.get("PYTHON_SECRET")
 
 # Set the source repository and file path in the main branch
 source_repo = "XXX"
-source_file_path = "sonar.properties"
+source_file_path = "sonar.properties"  # Just the filename
 
 def get_repos():
     url = f"https://api.github.com/orgs/{organization}/repos"
@@ -27,13 +27,14 @@ def get_file_content(repo, file_path):
     }
 
     response = requests.get(url, headers=headers)
-    content = response.json().get("content")
-    if content:
-        return base64.b64decode(content).decode("utf-8")
+    if response.status_code == 200:
+        content = response.json().get("content")
+        if content:
+            return base64.b64decode(content).decode("utf-8")
     return None
 
 def add_sonar_properties(repo, properties_content):
-    url = f"https://api.github.com/repos/{organization}/{repo['name']}/contents/sonar.properties"
+    url = f"https://api.github.com/repos/{organization}/{repo}/contents/sonar.properties"
     headers = {
         "Authorization": f"Bearer {access_token}"
     }
@@ -46,15 +47,21 @@ def add_sonar_properties(repo, properties_content):
 
     response = requests.put(url, json=data, headers=headers)
     if response.status_code == 201:
-        print(f"SonarQube properties added to {repo['name']}")
+        print(f"SonarQube properties added to {repo}")
     else:
-        print(f"Failed to add SonarQube properties to {repo['name']}: {response.text}")
+        print(f"Failed to add SonarQube properties to {repo}: {response.text}")
 
 if __name__ == "__main__":
     source_properties_content = get_file_content(source_repo, source_file_path)
     if source_properties_content is None:
         print(f"Failed to fetch SonarQube properties from {source_repo}")
     else:
+        print("Source properties content:")
+        print(source_properties_content)
         repos = get_repos()
+        print(f"Number of repos: {len(repos)}")
         for repo in repos:
-            add_sonar_properties(repo, source_properties_content)
+            if isinstance(repo, dict) and 'name' in repo:
+                repo_name = repo['name']
+                print(f"Adding properties to repo: {repo_name}")
+                add_sonar_properties(repo_name, source_properties_content)
