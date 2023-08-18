ddcdd97e8435aa223de1ddc931622bfd95c42ad1
Prasanna-source31 <139847095+Prasanna-source31@users.noreply.github.com>
2023-08-18 16:11:43 +0530

    Update add_sonar_properties.py

--- add_sonar_properties.py
+++ add_sonar_properties.py
@@ -6,9 +6,7 @@ import os
 organization = "Prasanna-source31"
 access_token = os.environ.get("PYTHON_SECRET")
 
-# Set the source repository and file path in the main branch
-source_repo = "XXX"
-source_file_path = "sonar.properties"
+source_file_url = 'https://github.com/Prasanna-source31/XXX/blob/main/sonar.properties'
 
 def get_repos():
     url = f"https://api.github.com/orgs/{organization}/repos"
@@ -19,18 +17,6 @@ def get_repos():
     response = requests.get(url, headers=headers)
     repos = response.json()
     return repos
-
-def get_file_content(repo, file_path):
-    url = f"https://api.github.com/repos/{organization}/{repo}/contents/{file_path}"
-    headers = {
-        "Authorization": f"Bearer {access_token}"
-    }
-
-    response = requests.get(url, headers=headers)
-    content = response.json().get("content")
-    if content:
-        return base64.b64decode(content).decode("utf-8")
-    return None
 
 def add_sonar_properties(repo, properties_content):
     url = f"https://api.github.com/repos/{organization}/{repo['name']}/contents/sonar.properties"
@@ -51,10 +37,11 @@ def add_sonar_properties(repo, properties_content):
         print(f"Failed to add SonarQube properties to {repo['name']}: {response.text}")
 
 if __name__ == "__main__":
-    source_properties_content = get_file_content(source_repo, source_file_path)
-    if source_properties_content is None:
-        print(f"Failed to fetch SonarQube properties from {source_repo}")
-    else:
+    properties_response = requests.get(source_file_url)
+    if properties_response.status_code == 200:
+        source_properties_content = properties_response.text
         repos = get_repos()
         for repo in repos:
             add_sonar_properties(repo, source_properties_content)
+    else:
+        print(f"Failed to fetch SonarQube properties from {source_file_url}")
