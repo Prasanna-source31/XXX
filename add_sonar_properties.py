c6504190df051fc27386fc6dc79a3902228539d8
Prasanna-source31 <139847095+Prasanna-source31@users.noreply.github.com>
2023-08-21 16:43:15 +0530

    Update add_sonar_properties.py

--- add_sonar_properties.py
+++ add_sonar_properties.py
@@ -1,18 +1,25 @@
 import requests
-
+token_variable = "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"
 def get_org_repos(org_name):
   url = "https://api.github.com/orgs/{}/repos".format(org_name)
-  headers = {"Authorization": "bearer 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"}
+  headers = {"Authorization": "bearer {}".format(token_variable)}
   response = requests.get(url, headers=headers)
+  return response.json()
 
-  if response.status_code == 200:
-    return response.json()
-  else:
-    raise Exception("Error getting repos: {}".format(response.status_code))
+def add_file_to_repos(file_path, org_name, repo_name):
+  repos = get_org_repos(org_name)
+  for repo in repos:
+    if repo_name in repo:
+      print("Repo: {}".format(repo["name"]))
+      repo_url = "https://github.com/{}/{}".format(org_name, repo["name"])
+      repo_clone_url = repo["clone_url"]
+      cloned_repo = git.clone(repo_clone_url)
+      git.add(file_path, cloned_repo)
+      git.commit(message="Adding file to all repos", repo=cloned_repo)
+      git.push(cloned_repo)
 
+file_path = "sonar.properties"
 org_name = "Prasanna-source31"
+repo_name = "XXX"
 
-repos = get_org_repos(org_name)
-
-for repo in repos:
-  print(repo["name"])
+add_file_to_repos(file_path, org_name, repo_name)
