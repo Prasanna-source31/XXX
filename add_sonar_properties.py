a9d137e5d03d5d89d70f43fe61389be6454d31f0
Prasanna-source31 <139847095+Prasanna-source31@users.noreply.github.com>
2023-08-22 09:47:58 +0530

    Update add_sonar_properties.py

--- add_sonar_properties.py
+++ add_sonar_properties.py
@@ -1,102 +1,68 @@
-"""
-Program to Copy a File to All Repositories in an Organization
-
-This program provides a function to copy a file from a GitHub repository to all other repositories in an organization.
-
-"""
-
-import logging
 import requests
+import base64
 import os
 
-# Setting up logging to monitor performance and errors
-logging.basicConfig(level=logging.INFO)
+# Set your GitHub organization name and access token
+organization = "Prasanna-source31"
+#access_token = os.environ.get("PYTHON_SECRET
+#access_token = "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"
 
-def copy_file_to_repos(org_name: str, repo_name: str, file_path: str, github_token: str) -> None:
-    """
-    Copy a File to All Repositories in an Organization
+# Set the source repository and file path in the main branch
+source_repo = "XXX"
+source_file_path = "sonar.properties"  # Just the filename
 
-    This function copies a file from a GitHub repository to all other repositories in an organization.
+def get_repos():
+    url = f"https://api.github.com/orgs/{organization}/repos"
+    headers = {
+        "Authorization": f"Bearer {GITHUB_TOKEN}"
+    }
 
-    Args:
-    org_name (str): Name of the organization.
-    repo_name (str): Name of the repository containing the file.
-    file_path (str): Path of the file to be copied.
-    github_token (str): GitHub personal access token with appropriate permissions.
+    response = requests.get(url, headers=headers)
+    repos = response.json()
+    return repos
 
-    Returns:
-    None
+def get_file_content(repo, file_path):
+    url = f"https://api.github.com/repos/{organization}/{repo}/contents/{file_path}"
+    headers = {
+        "Authorization": f"Bearer {GITHUB_TOKEN}"
+    }
 
-    Raises:
-    ValueError: If any of the input arguments are empty or invalid.
-    Exception: If an error occurs during the file copy process.
+    response = requests.get(url, headers=headers)
+    if response.status_code == 200:
+        content = response.json().get("content")
+        if content:
+            return base64.b64decode(content).decode("utf-8")
+    return None
 
-    Examples:
-    >>> copy_file_to_repos("myorg", "myrepo", "path/to/file.txt", "mytoken")
-    INFO: Fetching repositories in the organization...
-    INFO: Found 10 repositories in the organization.
-    INFO: Copying file to repository: repo1
-    INFO: File copied successfully to repository: repo1
-    INFO: Copying file to repository: repo2
-    INFO: File copied successfully to repository: repo2
-    ...
-    INFO: Copying file to repository: repo10
-    INFO: File copied successfully to repository: repo10
-    INFO: File copied to all repositories in the organization.
+def add_sonar_properties(repo, properties_content):
+    url = f"https://api.github.com/repos/{organization}/{repo}/contents/sonar.properties"
+    headers = {
+        "Authorization": f"Bearer {GITHUB_TOKEN}"
+    }
 
-    """
+    encoded_content = base64.b64encode(properties_content.encode("utf-8")).decode("utf-8")
+    data = {
+        "message": "Add SonarQube properties",
+        "content": encoded_content
+    }
 
-    # Validate input arguments
-    if not org_name or not repo_name or not file_path or not github_token:
-        raise ValueError("Invalid input arguments. Please provide valid values for org_name, repo_name, file_path, and github_token.")
-
-    try:
-        logging.info("Fetching repositories in the organization...")
-        # Fetch all repositories in the organization
-        headers = {
-            "Authorization": f"Bearer {github_token}"
-        }
-        url = f"https://api.github.com/orgs/{org_name}/repos"
-        response = requests.get(url, headers=headers)
-        response.raise_for_status()
-        repositories = response.json()
-
-        logging.info(f"Found {len(repositories)} repositories in the organization.")
-
-        # Copy the file to each repository
-        for repository in repositories:
-            repo_full_name = repository["full_name"]
-            logging.info(f"Copying file to repository: {repo_full_name}")
-
-            # Create a new file in the repository with the contents of the original file
-            url = f"https://api.github.com/repos/{repo_full_name}/contents/{file_path}"
-            data = {
-                "message": "Copy file to repository",
-                "content": "",
-                "sha": "",
-                "branch": "main"
-            }
-            response = requests.put(url, headers=headers, json=data)
-            response.raise_for_status()
-
-            logging.info(f"File copied successfully to repository: {repo_full_name}")
-
-        logging.info("File copied to all repositories in the organization.")
-
-    except Exception as e:
-        logging.error(f"An error occurred: {e}")
-        raise
+    response = requests.put(url, json=data, headers=headers)
+    if response.status_code == 201:
+        print(f"SonarQube properties added to {repo}")
+    else:
+        print(f"Failed to add SonarQube properties to {repo}: {response.text}")
 
 if __name__ == "__main__":
-    # Example usage
-    org_name = "Prasanna-source31"
-    repo_name = "XXX"
-    file_path = "sonar.properties"
-    github_token = "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"
-
-    try:
-        copy_file_to_repos(org_name, repo_name, file_path, github_token)
-    except ValueError as ve:
-        print(f"Invalid input arguments: {ve}")
-    except Exception as e:
-        print(f"An error occurred: {e}")
+    source_properties_content = get_file_content(source_repo, source_file_path)
+    if source_properties_content is None:
+        print(f"Failed to fetch SonarQube properties from {source_repo}")
+    else:
+        print("Source properties content:")
+        print(source_properties_content)
+        repos = get_repos()
+        print(f"Number of repos: {len(repos)}")
+        for repo in repos:
+            if isinstance(repo, dict) and 'name' in repo:
+                repo_name = repo['name']
+                print(f"Adding properties to repo: {repo_name}")
+                add_sonar_properties(repo_name, source_properties_content)
